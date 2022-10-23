ee01f2c572495c9dd712f22f52410bad81162c5b
Emreuss <evcimenemre8@gmail.com>
2022-10-23 15:43:07 +0300

    t_sql.sql file added

---
+++ t_sql.sql
@@ -0,0 +1,9 @@
+CREATE VIEW vw_PersonalInformation AS
+SELECT CONCAT_WS(' ',P_NAME,P_SURNAME) AS "FULL_NAME",
+       CASE 
+       WHEN P_GENDER=0 THEN 'Erkek'
+       WHEN P_GENDER=1 THEN 'Kadın'
+       ELSE 'Belirtilmedi' END AS "Cinsiyet",
+       P_BIRTHPLACE,
+       P_TCNUMBER
+FROM PERSONAL.HR.PERSON;
