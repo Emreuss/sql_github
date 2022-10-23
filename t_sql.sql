586027623ff860e287c48823f348cca018e1882a
Emreuss <evcimenemre8@gmail.com>
2022-10-23 15:48:58 +0300

    t_sql.sql file modified

--- t_sql.sql
+++ t_sql.sql
@@ -7,3 +7,15 @@ SELECT CONCAT_WS(' ',P_NAME,P_SURNAME) AS "FULL_NAME",
        P_BIRTHPLACE,
        P_TCNUMBER
 FROM PERSONAL.HR.PERSON;
+ALTER VIEW vw_PersonalInformation AS
+SELECT CONCAT_WS(' ',P_NAME,P_SURNAME) AS "FULL_NAME",
+       CASE
+       WHEN P_GENDER=0 THEN 'Erkek'
+       WHEN P_GENDER=1 THEN 'Kadın'
+       ELSE 'Belirtilmedi' END AS "Cinsiyet",
+       P_BIRTHPLACE,
+       P_TCNUMBER,
+       P_SALARY
+FROM PERSONAL.HR.PERSON
+
+SELECT * FROM vw_PersonalInformation
